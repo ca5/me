7c553a6c26819c79a5ee4cd12846c17089d7e802
Ca5 <ca54makske@yahoo.co.jp>
2025-11-03 18:46:43 +0900

    discographyのjson化

--- next-app/src/app/discography/page.tsx
+++ next-app/src/app/discography/page.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import fs from 'fs';
+import path from 'path';
+
+type DiscographyItem = {
+  title: string;
+  type: string;
+  description: string;
+  url: string;
+  imageUrl: string | null;
+};
+
+type DiscographyData = {
+  [year: string]: DiscographyItem[];
+};
 
 export default function Discography() {
+  const discographyPath = path.join(process.cwd(), 'src', 'app', 'discography', 'discography.json');
+  let discographyData: DiscographyData = {};
+
+  try {
+    const fileContents = fs.readFileSync(discographyPath, 'utf8');
+    discographyData = JSON.parse(fileContents);
+  } catch (error) {
+    console.error('Error reading or parsing discography.json:', error);
+    return <p>Error loading discography data.</p>;
+  }
+
+  const years = Object.keys(discographyData).sort((a, b) => parseInt(b) - parseInt(a));
+
   return (
     <>
       <div className="p-5 mb-4 mainvisual">
@@ -31,271 +58,26 @@ export default function Discography() {
           </div>
         </div>
 
-        <h2>2016</h2>
-        <div className="row">
-          <div className="col-md-4 col-xs-12">
-            <h3>ESC Sound Collections</h3>
-            <h4>Compilation (#1 New World/Ca5)</h4>
-            <a href="https://esctrax.bandcamp.com/album/esc-ex004-esc-sound-collection">
-              <img src="/img/discography/ESCSC.jpg" alt="" width="300px" height="300px" />
-            </a>
+        {years.map((year) => (
+          <div key={year}>
+            <h2>{year}</h2>
+            <div className="row">
+              {discographyData[year].map((item, index) => (
+                <div className="col-md-4 col-xs-12" key={index}>
+                  <h3>{item.title}</h3>
+                  <h4>{item.type} {item.description && `(${item.description})`}</h4>
+                  <a href={item.url}>
+                    {item.imageUrl ? (
+                      <img src={item.imageUrl} alt={item.title} width="300px" height="300px" />
+                    ) : (
+                      'Link'
+                    )}
+                  </a>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Chip Break In The Clouds</h3>
-            <h4>Compilation (#11 Cassis and Soda/Ca5)</h4>
-            <a href="https://bleeplove.bandcamp.com/album/chip-break-in-the-clouds">
-              <img src="/img/discography/CBITC.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Back to the 8bit</h3>
-            <h4>Album</h4>
-            <a href="https://esctrax.bandcamp.com/album/esc-003-back-to-the-8bit">
-              <img src="/img/discography/BTT8.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>NewYearTracks2016</h3>
-            <h4>Compilation (robinson(robi-chika mix)/Ca5)</h4>
-            <a href="http://ameblo.jp/shcblog/entry-12114136069.html">
-              <img src="/img/discography/NYT2016.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-        </div>
-
-        <h2>2015</h2>
-        <div className="row">
-          <div className="col-md-4 col-xs-12">
-            <h3>つくしLP</h3>
-            <h4>Compilation (#9 YAMADA Show 10/Ca5)</h4>
-            <a href="http://tuxurecords.tumblr.com/post/135106243736/%E3%81%A4%E3%81%8F%E3%81%97lp">
-              <img src="http://66.media.tumblr.com/b8e84bae25f57bb5d51914d11f083846/tumblr_inline_nz7jc1E9BD1qes5f7_540.jpg" alt="" width="300px" height="300px"/>
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>DYSTOPIA</h3>
-            <h4>Album</h4>
-            <a href="https://esctrax.bandcamp.com/album/esc-ex002-dystopia">
-              <img src="/img/discography/DYSTOPIA.jpg" width="300px" height="300px" alt="dystopia"/>
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>DMGRMXS</h3>
-            <h4>Compilation (#1 オープニングBGM(ロックマン3), #3 時の回廊(クロノトリガー)/Ca5)</h4>
-            <a href="https://esctrax.bandcamp.com/album/esc-ex003-dmg-rmxs">
-              <img src="/img/discography/DMGRMXS.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Raindrop</h3>
-            <h4>Remix (#3 Raindrop(Ca5 Remix)/Ca5)</h4>
-            <a href="https://esctrax.bandcamp.com/album/esc-ex001-raindrop-ep">
-              <img src="/img/discography/Raindrop.jpg" width="300px" height="300px" alt="raindrop"/>
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>After 5 EP</h3>
-            <h4>EP</h4>
-            <a href="https://bleeplove.bandcamp.com/album/after-5-ep">
-              <img src="http://blog.ca5.me/images/20150806/jacket.jpg"
-           width="300px" height="300px" alt="after 5 ep"/>
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Chip Juke Bros.</h3>
-            <h4>Compilation (#5 chica5)</h4>
-            <a href="https://parallelogramrecords.bandcamp.com/album/chip-juke-bros">
-              <img src="https://f1.bcbits.com/img/a2659409642_16.jpg" width="300px" height="300px" alt="chip juke bros"/>
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>つくしEP6</h3>
-            <h4>Compilation (#8 Be There(big up mix))</h4>
-            <a href="http://tuxurecords.tumblr.com/post/105860816816/%E3%81%A4%E3%81%8F%E3%81%97ep6">
-            <img src="http://33.media.tumblr.com/b6302d5ab0d2a795214aa1394a50d798/tumblr_inline_ngxvplxJzB1qes5f7.jpg" width="300px" height="300px" alt="tukushi ep6"/>
-            </a>
-          </div>
-        </div>
-
-        <h2>2014</h2>
-        <div className="row">
-          <div className="col-md-4 col-xs-12">
-            <h3>digital </h3>
-            <h4>Compilation (#11 dial-up surfer/Ca5)</h4>
-            <a href="http://pitifulpippuppet.jp/album/digital/download.html">
-              <img src="http://pitifulpippuppet.jp/album/digital/cover400.gif" width="300px" height="300px" alt="digital"/>
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>TripleX -LemonsquashExtra01-</h3>
-            <h4>Compilation (#10 EXOSC/Ca5)</h4>
-            <a href="http://sm.2-d.jp/blog/2014/04/whcd0019triplex--lemonsquashextra01-.html">
-              <img src="http://sm.2-d.jp/cds/whcd0019.jpg" width="300px" height="300px" alt="triplex"/>
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>CAPCORE vs NKRN</h3>
-            <h4>Compilation (#2 megafire/Ca5)</h4>
-            <a href="http://nkrn.bandcamp.com/album/v-a-capcore-vs-nkrn">
-              <img src="http://f1.bcbits.com/img/a1340224891_2.jpg" width="300px" height="300px" alt="capcore vs nkrn"/>
-            </a>
-          </div>
-        </div>
-
-        <h2>2013</h2>
-        <div className="row">
-          <div className="col-md-4 col-xs-12">
-            <h3>Memory of the Cartridge</h3>
-            <h4>Album</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN051">
-              <img src="http://www.otherman-records.com/images/releases/OTMN051.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Monster Tale</h3>
-            <h4>Compilation (#12 Summer Shores/Ca5)</h4>
-            <a href="https://archive.org/download/8081-va013">
-              archive.orgに音源残ってたのでこっそりリンク貼っておきます。。
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>NKRN SELECTION</h3>
-            <h4>Compilation (#2 mgmn3, #7 920(2013mix), #11 Snow Valley /Ca5)</h4>
-            <a href="https://www.discogs.com/ja/Various-NKRN-SELECTION-Game-Music-Covers-/release/5045578">
-              <img src="/img/discography/NKRNSL.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Last Summer EP</h3>
-            <h4>Compilation (#2 Suikawari/Ca5)</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN047">
-              <img src="http://www.otherman-records.com/images/releases/OTMN047.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>HEMISHINKU</h3>
-            <h4>Compilation (#2 symmetric/Ca5)</h4>
-            <a href="http://pitifulpippuppet.jp/album/hemishinku/download.html">
-              <img src="http://pitifulpippuppet.jp/pic/2013/hemi265.gif" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Deep Blue</h3>
-            <h4>EP</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN045">
-              <img src="http://www.archive.org/download/OTMN045/deepblue-jacket.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>iron</h3>
-            <h4>Compilation (#1 steel wool/Ca5)</h4>
-            <a href="http://pitifulpippuppet.web.fc2.com/album/iron.html">
-              <img src="http://pitifulpippuppet.web.fc2.com/pic/IRON400.gif" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-        </div>
-
-        <h2>2012</h2>
-        <div className="row">
-          <div className="col-md-4 col-xs-12">
-            <h3>Chiptunes Change Christmas</h3>
-            <h4>Compilation (#10 white illumination/Ca5)</h4>
-            <a href="http://www.otherman-records.com/releases/CHIP002">
-              <img src="http://www.otherman-records.com/special/CHIP002/images/jacket_on.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Cyber terrorism</h3>
-            <h4>Remix (#3 Gumblar(Ca5 remix)/Ca5)</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN037">
-              <img src="http://www.archive.org/download/OTMN037/OTMN037.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>贄 killer 9 (nie killer nine) #5</h3>
-            <h4>Compilation (#1 ふたりのボカロ/Ca5)</h4>
-            <a href="https://nkrn.bandcamp.com/album/killer-9-nie-killer-nine-5">
-              <img src="/img/discography/NKRN5.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Nihon Rave EP</h3>
-            <h4>Remix (#4 Hemoro X(Ca5 remix)/Ca5)</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN030">
-              <img src="http://www.archive.org/download/OTMN030/jacket.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>THE DOPEMAN SHOW!</h3>
-            <h4>Compilation (#12 whiteout/Ca5)</h4>
-            <a href="http://www.otherman-records.com/releases/DPMN001">
-              <img src="http://www.archive.org/download/DPMN001/jacket.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-        </div>
-
-        <h2>2011</h2>
-        <div className="row">
-          <div className="col-md-4 col-xs-12">
-            <h3>贄 killer 9 (nie killer nine) #4</h3>
-            <h4>Compilation (#3 Snowy Vallay/Ca5)</h4>
-            <a href="https://nkrn.bandcamp.com/album/killer-9-nie-killer-nine-4">
-              <img src="/img/discography/NKRN4.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>Grayscale</h3>
-            <h4>EP</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN024">
-              <img src="http://www.archive.org/download/OTMN024/folder.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>color EP</h3>
-            <h4>Compilation (#2 evergreen,#4 U-YAKE walk&apos;in(Ca5 remix)/Ca5)</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN010">
-              <img src="http://www.archive.org/download/OTMN010/folder.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>9in1 (Ca5 classics)</h3>
-            <h4>Album</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN017">
-              <img src="http://www.archive.org/download/OTMN017/9in1.png" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-        </div>
-        <h2>2010</h2>
-        <div className="row">
-          <div className="col-md-4 col-xs-12">
-            <h3>贄 killer 9 (nie killer nine) #3</h3>
-            <h4>Compilation (#1 mgmn/Ca5)</h4>
-            <a href="https://nkrn.bandcamp.com/album/killer-9-nie-killer-nine-3">
-              <img src="/img/discography/NKRN3.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>贄 killer 9 (nie killer nine) #2</h3>
-            <h4>Compilation (#1 920/Ca5)</h4>
-            <a href="https://nkrn.bandcamp.com/album/killer-9-nie-killer-nine-2">
-              <img src="/img/discography/NKRN2.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>songs 4 the man</h3>
-            <h4>Compilation (#9 #50/Ca5)</h4>
-            <a href="http://www.otherman-records.com/releases/VL4OTMN_001">
-              <img src="http://www.otherman-records.com/special/vl4otmn_001/images/jacket.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <h3>The Otherman Showcase</h3>
-            <h4>Compilation (#1 cyberSP/Ca5)</h4>
-            <a href="http://www.otherman-records.com/releases/OTMN001">
-              <img src="http://www.archive.org/download/OTMN001/otmn001_big.jpg" alt="" width="300px" height="300px" />
-            </a>
-          </div>
-        </div>
+        ))}
 
         <h2>2009以前</h2>
         <div className="row">
